91fc776ca369f1e84e99b25f5d71d120dd99fc96
ASIA11793 <1439083552@qq.com>
2019-11-18 08:01:14 +0800

    jjj

--- app/src/main/java/com/example/listviewexample/MyAdapter.java
+++ app/src/main/java/com/example/listviewexample/MyAdapter.java
@@ -5,6 +5,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,9 +15,16 @@ import java.util.List;
 
 public class MyAdapter extends ArrayAdapter<Player> {
     List<Player> listOfPlayers;
+<<<<<<< Updated upstream
+=======
+    Context context;
+
+
+>>>>>>> Stashed changes
     public MyAdapter(@NonNull Context context, int resource, @NonNull List<Player> objects) {
         super(context, resource, objects);
         listOfPlayers = objects;
+        this.context=context;
     }
 
     @NonNull
@@ -26,6 +35,12 @@ public class MyAdapter extends ArrayAdapter<Player> {
         LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         if (single_item_view == null)
             single_item_view = inflater.inflate(R.layout.single_item, null);
+        TextView name=(TextView) single_item_view.findViewById(R.id.name);
+        TextView age=(TextView) single_item_view.findViewById(R.id.age);
+        TextView money=(TextView) single_item_view.findViewById(R.id.money);
+        TextView sport=(TextView) single_item_view.findViewById(R.id.sport);
+        ImageView ima=(ImageView) single_item_view.findViewById(R.id.ima);
+
         //Todo get single player using position and listOfPlayers
         // get references to views in single_item.xml , for example
         //        TextView name = single_item_view.findViewById(R.id.name);
